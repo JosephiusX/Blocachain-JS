31fbd0977f36745d424cd725efed3752d15a607a
Joseph <josephiusx@gmail.com>
2021-11-07 19:28:43 -0800

    39.   Building "POST/register-and-broadcast-node" Endpoint - Part 2

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -57,11 +57,20 @@ app.post('/register-and-broadcast-node', function (req, res) {
 
 		regNodesPromises.push(rp(requestOptions))
 	});
+
 	Promise.all(regNodesPromises)
 	.then(data => {
-		// ...
-		// use the data....
-	});
+		const bulkRegisterOptions = {
+			uri: newNodeUrl + '/register-nodes-bulk',
+			method: 'POST',
+			body: { allNetworkNodes: [ ...bitcoin.networkNodes, bitcoin.currentNodeUrl ]},
+			json: true
+		};
+
+		return rp(bulkRegisterOptions);
+	}).then(data => {
+		res.json({ note: 'New node registered with network successfully.'})
+	})
 });
 
 // register a node with the network
